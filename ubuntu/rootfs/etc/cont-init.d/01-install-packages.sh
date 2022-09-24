1a1f0e38705dcc0aa87bdd86b6bb416e93caf4c8
coderadu <47945947+coderadu@users.noreply.github.com>
2022-09-24 14:41:30 +0000

    Changed to versions

--- ubuntu/rootfs/etc/cont-init.d/01-install-packages.sh
+++ ubuntu/rootfs/etc/cont-init.d/01-install-packages.sh
@@ -2,15 +2,31 @@
 # shellcheck shell=bash
 
 REPO="https://raw.githubusercontent.com/coderadu/hassio-ubuntu-packages/main"
-INSTALL_SCRIPT="install.sh"
 
 if [ ! -d /data/packages ]; then
   mkdir -p /data/packages
 fi
 
 for package in $(bashio::config "packages"); do
-  bashio::log.info "Install ${package}"
-  URL="$REPO/${package}/$INSTALL_SCRIPT"
-  bashio::log.info "Get $URL"
-  curl -o- "$URL" | bash
+  NAME=$(jq -r '.name' <<< "${package}")
+  VERSION=$(jq -r '.version' <<< "${package}")
+  if [ "$VERSION" == "latest" ]; then
+    VERSION=$(curl -o- "$REPO/$NAME/latest")
+  fi
+  if [ -d "/data/packages/$NAME" ]; then
+    INSTALLED_VERSION=$(cat "/data/packages/$NAME/pkg_version") || "0"
+  else
+    INSTALLED_VERSION="0"
+  fi
+  if [ ! "$VERSION" == "$INSTALLED_VERSION" ]; then
+    rm -rf "/data/packages/$NAME"
+    bashio::log.info "Install $NAME version $VERSION"
+    mkdir -p "/data/packages/$NAME"
+    wget "$REPO/$NAME/$NAME-$VERSION.tar" -O /tmp/package.tar
+    tar -xf /tmp/package.tar -C "/data/packages/$NAME"
+    rm /tmp/package.tar
+    echo "$VERSION" > "/data/packages/$NAME/pkg_version"
+    bash "/data/packages/$NAME/postinstall.sh"
+    bashio::log.info "$NAME successfully installed"
+  fi
 done
