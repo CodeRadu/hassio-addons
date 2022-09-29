4829ccac9ac5d9c2f2c4e723570bf1096f731581
coderadu <47945947+coderadu@users.noreply.github.com>
2022-09-29 19:12:24 +0300

    Changes to packages

--- ubuntu/rootfs/etc/cont-init.d/01-install-packages.sh
+++ ubuntu/rootfs/etc/cont-init.d/01-install-packages.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/with-contenv bashio
 # shellcheck shell=bash
 
-REPO="https://raw.githubusercontent.com/coderadu/hassio-ubuntu-packages/main"
-
 if [ ! -d /data/packages ]; then
   mkdir -p /data/packages
 fi
@@ -10,23 +8,5 @@ fi
 for package in $(bashio::config "packages"); do
   NAME=$(jq -r '.name' <<< "${package}")
   VERSION=$(jq -r '.version' <<< "${package}")
-  if [ "$VERSION" == "latest" ]; then
-    VERSION=$(curl -o- "$REPO/$NAME/latest")
-  fi
-  if [ -d "/data/packages/$NAME" ]; then
-    INSTALLED_VERSION=$(cat "/data/packages/$NAME/pkg_version") || "0"
-  else
-    INSTALLED_VERSION="0"
-  fi
-  if [ ! "$VERSION" == "$INSTALLED_VERSION" ]; then
-    rm -rf "/data/packages/$NAME"
-    bashio::log.info "Install $NAME version $VERSION"
-    mkdir -p "/data/packages/$NAME"
-    wget "$REPO/$NAME/$NAME-$VERSION.tar" -O /tmp/package.tar
-    tar -xf /tmp/package.tar -C "/data/packages/$NAME"
-    rm /tmp/package.tar
-    echo "$VERSION" > "/data/packages/$NAME/pkg_version"
-    bash "/data/packages/$NAME/postinstall.sh"
-    bashio::log.info "$NAME successfully installed"
-  fi
+  pkg install "$NAME" "$VERSION"
 done
--- ubuntu/rootfs/etc/cont-init.d/02-run-onstart.sh
+++ ubuntu/rootfs/etc/cont-init.d/02-run-onstart.sh
@@ -4,6 +4,11 @@
 CONFIG="/config/ubuntu"
 
 if [ -f "$CONFIG/onstart.sh" ]; then
+  cd /data/packages
+  for d in */; do
+    chmod a+x "$d/onstart.sh"
+    bash "$d/onstart.sh"
+  done
   chmod +x "$CONFIG/onstart.sh"
   "$CONFIG/onstart.sh"
 fi
