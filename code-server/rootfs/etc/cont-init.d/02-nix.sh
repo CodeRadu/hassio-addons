852533b55fa3e45d0f6ac26af8c83f8f7e52452f
Radu <radu.gabi.m@gmail.com>
2023-02-19 17:19:02 +0000

    remove nix

--- code-server/rootfs/etc/cont-init.d/02-nix.sh
+++ code-server/rootfs/etc/cont-init.d/02-nix.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/bash
 
-mkdir /data/nix
-mkdir /nix
+# mkdir /data/nix
+# mkdir /nix
 
-mount --bind /data/nix /nix
+# mount --bind /data/nix /nix
 
-if [ ! -d /nix/store ]; then
-  HOME=/root NIX_IGNORE_SYMLINK_STORE=1 sh <(curl -L https://nixos.org/nix/install) --daemon
-fi
+# if [ ! -d /nix/store ]; then
+#   HOME=/root NIX_IGNORE_SYMLINK_STORE=1 sh <(curl -L https://nixos.org/nix/install) --daemon
+# fi
 
-useradd nixbld1 --system
-groupadd nixbld
-usermod -aG nixbld nixbld1
+# useradd nixbld1 --system
+# groupadd nixbld
+# usermod -aG nixbld nixbld1
